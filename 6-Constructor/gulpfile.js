e95a66c72e6df7fc871ba993fa802c2820b1dbea
sswiatoslaw <sviatoslav.sodel@creditkasa.ua>
2023-01-22 14:30:13 +0200

    add constructor folder

--- 6-Constructor/gulpfile.js
+++ 6-Constructor/gulpfile.js
@@ -8,40 +8,46 @@ const sass = require('gulp-sass')(require('sass'));
 
 const buildHtml = () => {
     return gulp.src(['app/*.html', 'app/pages/**/*.html'])
-    .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/'));
 }
 
 const buildStyles = () => {
     return gulp.src('app/css/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('build/'))
-    .pipe(browserSync.stream());
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('build/'))
+        .pipe(browserSync.stream());
 }
 
 const buildScript = () => {
     return gulp.src('app/js/*.js')
-    .pipe(concat('script.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('build/'))
-    .pipe(browserSync.stream())
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/'))
+        .pipe(browserSync.stream())
+}
+
+const buildImages = () => {
+    return gulp.src('app/images/*.png')
+        .pipe(gulp.dest('build/images/'))
 }
 
 const build = () => {
     buildHtml();
     buildScript();
     buildStyles();
+    buildImages();
 }
 
 const start = () => {
     build();
     browserSync.init({
         server: {
-            baseDir: '/'
+            baseDir: './build'
         }
     })
-    gulp.watch('app/js/**/*.js', buildScript);
-    gulp.watch('app/css/**/*.scss', buildStyles);
+    gulp.watch('app/js/**/*.js', buildScript).on('change', browserSync.reload);
+    gulp.watch('app/css/**/*.scss', buildStyles).on('change', browserSync.reload);
     gulp.watch(['app/pages/**/*.html', 'app/index.html'], buildHtml).on('change', browserSync.reload);
 }
 
